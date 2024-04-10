2fee72ea7a73e455adb665205f507912ec1826df
shauray8 <shauray9@gmail.com>
2024-04-10 23:43:41 +0530

    move to a more forgiving distance

--- src/diffusers/pipelines/stable_diffusion/safety_checker.py
+++ src/diffusers/pipelines/stable_diffusion/safety_checker.py
@@ -28,6 +28,10 @@ def cosine_distance(image_embeds, text_embeds):
     normalized_text_embeds = nn.functional.normalize(text_embeds)
     return torch.mm(normalized_image_embeds, normalized_text_embeds.t())
 
+def jaccard_distance(image_embeds, text_embeds):
+    normalized_image_embeds = nn.functional.normalize(image_embeds)
+    normalized_text_embeds = nn.functional.normalize(text_embeds)
+    
 
 class StableDiffusionSafetyChecker(PreTrainedModel):
     config_class = CLIPConfig
