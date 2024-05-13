e2e88a1260f59279a6831cf266be8044b856777b
shauray8 <shauray9@gmail.com>
2024-05-13 10:23:34 +0530

    whitespcae

--- src/diffusers/plus_pipelines/xadapters/pipeline_controlnet_sd_xl.py
+++ src/diffusers/plus_pipelines/xadapters/pipeline_controlnet_sd_xl.py
@@ -1,4 +1,4 @@
- Copyright 2023 The HuggingFace Team. All rights reserved.
+#Copyright 2023 The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 # Copied directly from https://github.com/showlab/X-Adapter/blob/main/pipeline/pipeline_sd_xl_adapter_controlnet.py
 
 import inspect
